0f4a4c8fe5e07a94370f94f8153128a37cbc8e46
m-ert <mertekincmp@gmail.com>
2023-04-07 13:54:57 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:voyager_v01/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+//git deneme deneme01
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
