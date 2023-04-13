2e50a4853ee5f2faa2447c1a1c46c4cf2b77d08b
suleymanberkay <bloadraynex@gmail.com>
2023-04-13 22:34:28 +0300

    login/register

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,48 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:voyager_v01/auth.dart';
 import 'package:flutter/material.dart';
-import 'package:voyager_v01/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:voyager_v01/pages/home_page.dart';
+import 'package:voyager_v01/pages/login_page.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterScreenState extends State<RegisterScreen> {
   String? errorMessage = '';
   bool isLogin = true;
-
+  final TextEditingController _controllerUserName = TextEditingController();
   final TextEditingController _controllerEmail = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
-
-  Future<void> createUserWithEmailAndPassword() async {
-    try {
-      await Auth().createUserWithEmailAndPassword(
-        email: _controllerEmail.text,
-        password: _controllerPassword.text,
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
-    }
-  }
-
-  Future<void> signInWithEmailAndPassword() async {
-    try {
-      await Auth().signInWithEmailAndPassword(
-        email: _controllerEmail.text,
-        password: _controllerPassword.text,
-      );
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        errorMessage = e.message;
-      });
-    }
-  }
-
+  final TextEditingController _controllerPasswordAgain = TextEditingController();
   
   @override
   Widget build(BuildContext context) {
@@ -99,13 +75,7 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                           ),
                         ),
-                        SizedBox(
-                          width: 140,
-                        ),
-                        Text(
-                          'Forgot Password',
-                          style: TextStyle(color: Colors.grey),
-                        ),
+                        
                       ],
                     ),
                     SizedBox(height: 10,),
@@ -135,9 +105,56 @@ class _LoginPageState extends State<LoginPage> {
                     
                     
                     ),
-                    SizedBox(height: 80,),
+                    SizedBox(height: 30,),
+      Column(children: [
+          Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 35),
+                          child: Text(
+                            'Password Again',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        
+                      ],
+                    ),
+                    SizedBox(height: 10,),
+
+                    Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    child: Container(height: 65,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Padding(padding: EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 20),
+                      child: TextField(
+                        controller: _controllerPassword,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, hintText: 'Password Again'),
+
+                      ),
+                    ),
+                    ),
+                    ),
+                    ],
+                    
+                    
+                    
+                    ),
+                      SizedBox(height: 80,),
+
                     GestureDetector(
-                      onTap: (){
+                      onTap: (){Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
 
                       },
                       child: Padding(
@@ -154,7 +171,7 @@ class _LoginPageState extends State<LoginPage> {
                             borderRadius: BorderRadius.circular(58),
                           ),
                           child: Center(
-                            child: Text('Login',
+                            child: Text('Sign Up',
                             style:TextStyle(fontSize: 20,
                             color: Colors.orange,
                             fontWeight: FontWeight.bold,),),),
@@ -162,43 +179,8 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                     ),
                    SizedBox(height: 10,),
-        Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Do not have an account?',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),],),
-                    GestureDetector(
-                      onTap: (){ Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegisterScreen()));
-
-                      },
-                      child: Padding(
-                        padding:EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ), 
-                        
-                        child: Container(
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            border:Border.all(color:Colors.black),
-                            borderRadius: BorderRadius.circular(58),
-                          ),
-                          child: Center(
-                            child: Text('Sign up',
-                            style:TextStyle(fontSize: 20,
-                            color: Colors.orange,
-                            fontWeight: FontWeight.bold,),),),
-                        ),
-                        ),
-                    ),
+        
+                    
 
       ],),
       
@@ -211,45 +193,5 @@ class _LoginPageState extends State<LoginPage> {
       
     
   }
-
-
-Widget _title() {
-    return const Text('Voyager Auth');
-  }
-
-  Widget _entryField(
-    String title,
-    TextEditingController controller,
-  ) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(
-        labelText: title,
-      ),
-    );
-  }
-
-  Widget _errorMessage() {
-    return Text(errorMessage == '' ? '' : '$errorMessage');
-  }
-
-  Widget _submitButton() {
-    return ElevatedButton(
-      onPressed:
-          isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-      child: Text(isLogin ? 'Login' : 'Register'),
-    );
-  }
-
-  Widget _loginOrRegisterButton() {
-    return TextButton(
-      onPressed: () {
-        setState(() {
-          isLogin = !isLogin;
-        });
-      },
-      child: Text(isLogin ? 'Register instead' : 'Login instead'),
-    );
-  }
-
+  
 }
